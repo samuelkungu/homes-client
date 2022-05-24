fd48c539e96703895b64fddcb8a6ee9c16778cb0
Samuel <kungusamuel64@gmail.com>
2022-05-24 09:07:13 +0300

    featured properties

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,8 +1,36 @@
-import './featuredProperties.scss'
+import React from 'react'
+import "./featuredProperties.scss"
+import useFetch from '../../hooks/useFetch';
 
 function FeaturedProperties() {
+
+    const { data, loading, error } = useFetch("/hostels?featured=true&limit=4");
+
     return (
-        <div>FeaturedProperties</div>
+        <div className='fp'>
+
+            {loading ? ("Loading") : (
+                <>
+                    {data.map((item) => (
+                        <div className="fp-item" key={item._id} >
+                            <img
+                                src={item.photos[0]}
+                                alt=""
+                                className="fp-img"
+                            />
+                            <span className="fp-name">{item.name}</span>
+                            <span className="fp-area">{item.area}</span>
+                            <span className="fp-price">Starting from ${item.cheapestPrice}</span>
+                            {item.rating && <div className="fp-rating">
+                                <button>{item.rating}</button>
+                                <span>Excellent</span>
+                            </div>}
+                        </div>
+                    ))}
+
+                </>)}
+
+        </div>
     )
 }
 
